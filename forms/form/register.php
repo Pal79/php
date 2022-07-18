a61a54ff3b928cb1dc20b2e6acfb1afd01652fdf
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-18 22:02:57 +0200

    register.php modified

--- forms/form/register.php
+++ forms/form/register.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Pál Dániel">
         <meta name="description" content="from, űrlap, pratctice, gyakorlás">
-        <title>login</title>
+        <title>register</title>
     </head>
 
     <body>
