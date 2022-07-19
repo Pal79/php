d4a8f30907107d47569ae57d8e798f29e2292f9b
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-19 08:51:05 +0200

    index.php modified

--- forms/form/index.php
+++ forms/form/index.php
@@ -1,46 +1,59 @@
 <?php
 
-$nameErr=$userNameErr=$emailErr=$passwordErr=$progLangsErr="";
+require_once("register.php");
 
-if(isset($_POST["name"]) && !empty($_POST["name"])) {
-    $name = $_POST["name"];
-} else {
+$nameErr = "";
+$userNameErr = "";
+$emailErr = "";
+$passwordErr = "";
+$progLangsErr = "";
 
-}
-if(isset($_POST["userName"]) && !empty($_POST["userName"])) {
-    $userName = $_POST["userName"];
-} else {
-
-}
-if(isset($_POST["email"]) && !empty($_POST["email"])) {
-    $email = $_POST["email"];
-} else {
-
-}
-if(isset($_POST["password"]) && !empty($_POST["password"])) {
-    $password = $_POST["password"];
-} else {
-
-}
-if(isset($_POST["progLangs"]) && !empty($_POST["progLangs"])) {
-    $progLangs = $_POST["progLangs"];
-} else {
-
-}
-if(isset($_POST["forMySelf"])) {
-    $forMySelf = $_POST["forMySelf"];
-}
-if(isset($_POST["gender"])) {
-    $gender = $_POST["gender"];
-}
-if(isset($_POST["pets"])) {
-    $pets = $_POST["pets"];
-}
-if(isset($_POST["progLangs"])) {
-    $progLangs = $_POST["progLangs"];
+function stringTest($string) {
+    $string = trim($string);
+    $string = strip_tags($string);
+    $string = stripslashes($string);
+    return $string;
 }
 
-require_once("login.php");
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST["name"]) && !empty($_POST["name"]) && $_POST["name"] == stringTest($_POST["name"]) && preg_match("/^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ\s]$/u") && strlen($_POST["name"]) <= 40) {
+        $name = $_POST["name"];
+    } else {
+        $userNameErr = "Hibás név!!!";
+    }
+    if(isset($_POST["userName"]) && !empty($_POST["userName"]) && $_POST["name"] == stringTest($_POST["name"]) && preg_match("/^[a-zA-Z]*$/") && strlen($_POST["name"] <= 40)) {
+        $userName = $_POST["userName"];
+    } else {
+        $userNameErr = "Hibás felhasználónév!!!";
+    }
+    if(isset($_POST["email"]) && filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)) {
+        $email = $_POST["email"];
+    } else {
+        $emailErr = "Helytelen email cím!!!";
+    }
+    if(isset($_POST["password"]) && !empty($_POST["password"]) && $_POST["password"] == stringTest($_POST["password"]) && strlen($_POST["password"] <= 40) && strlen($_POST["password"]) > 10) {
+        $password = $_POST["password"];
+    } else {
+        $passwordErr = "Hibás jelszó!!!";
+    }
+    if(isset($_POST["progLangs"]) && !empty($_POST["progLangs"])) {
+        $progLangs = $_POST["progLangs"];
+    } else {
+        $progLangsErr = "Legalább egy nyelvet ki kell jelölnöd!!!";
+    }
+    if(isset($_POST["forMySelf"])) {
+        $forMySelf = $_POST["forMySelf"];
+    }
+    if(isset($_POST["gender"])) {
+        $gender = $_POST["gender"];
+    }
+    if(isset($_POST["pets"])) {
+        $pets = $_POST["pets"];
+    }
+    if(isset($_POST["progLangs"])) {
+        $progLangs = $_POST["progLangs"];
+    }
+}
 
 if(isset($name) && isset($userName) && isset($email) && isset($password) && isset($progLangs)){
     echo "Név: " . $name . "<br>";
