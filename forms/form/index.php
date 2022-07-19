ed13d267ecc31d018618bfb0cac65f33d333cab6
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-19 11:23:45 +0200

    index.php modified

--- forms/form/index.php
+++ forms/form/index.php
@@ -1,12 +1,6 @@
 <?php
 
-require_once("register.php");
-
-$nameErr = "";
-$userNameErr = "";
-$emailErr = "";
-$passwordErr = "";
-$progLangsErr = "";
+$nameErr = $userNameErr = $emailErr = $passwordErr = $progLangsErr = "";
 
 function stringTest($string) {
     $string = trim($string);
@@ -16,12 +10,12 @@ function stringTest($string) {
 }
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(isset($_POST["name"]) && !empty($_POST["name"]) && $_POST["name"] == stringTest($_POST["name"]) && preg_match("/^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ\s]*$/u") && strlen($_POST["name"]) <= 40) {
+    if(isset($_POST["name"]) && !empty($_POST["name"]) && $_POST["name"] == stringTest($_POST["name"]) && preg_match("/^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ\s]*$/u", $_POST["name"]) && strlen($_POST["name"]) <= 40) {
         $name = $_POST["name"];
     } else {
-        $userNameErr = "Hibás név!!!";
+        $nameErr = "Hibás név!!!";
     }
-    if(isset($_POST["userName"]) && !empty($_POST["userName"]) && $_POST["name"] == stringTest($_POST["name"]) && preg_match("/^[a-zA-Z]*$/") && strlen($_POST["name"] <= 40)) {
+    if(isset($_POST["userName"]) && !empty($_POST["userName"]) && $_POST["userName"] == stringTest($_POST["userName"]) && preg_match("/^[a-zA-Z\s\-]*$/", $_POST["userName"]) && strlen($_POST["userName"]) <= 40) {
         $userName = $_POST["userName"];
     } else {
         $userNameErr = "Hibás felhasználónév!!!";
@@ -31,7 +25,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $emailErr = "Helytelen email cím!!!";
     }
-    if(isset($_POST["password"]) && !empty($_POST["password"]) && $_POST["password"] == stringTest($_POST["password"]) && strlen($_POST["password"] <= 40) && strlen($_POST["password"]) > 10) {
+    if(isset($_POST["password"]) && !empty($_POST["password"]) && $_POST["password"] == stringTest($_POST["password"]) && strlen($_POST["password"]) <= 40 && strlen($_POST["password"]) > 10) {
         $password = $_POST["password"];
     } else {
         $passwordErr = "Hibás jelszó!!!";
@@ -55,14 +49,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
+require_once("register.php");
+
 if(isset($name) && isset($userName) && isset($email) && isset($password) && isset($progLangs)){
+    echo "<br><hr>";
     echo "Név: " . $name . "<br>";
     echo "Felhasználó név: " . $userName . "<br>";
     echo "Email: " . $email . "<br>";
     echo "Jelszó: " . $password . "<br>";
-    foreach($progLangs as $p) {
-        echo $p . "<br>";
-    }
     echo "Magamról: " . $forMySelf . "<br>";
     echo "Nemed: " . $gender . "<br>";
     echo "Háziállat: ";
